26604bd2d6facb715381d17940ceaf76c35738a0
sbaldu <simone.balducci00@gmail.com>
2024-02-12 12:13:22 +0100

    Improve plotting script

--- Mandelbrot/tbb/grainsize/plot.py
+++ Mandelbrot/tbb/grainsize/plot.py
@@ -3,6 +3,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import sys
 
 # Read in the data
 data = pd.read_csv('data.csv')
@@ -11,15 +12,19 @@ data = pd.read_csv('data.csv')
 grainsize = data['grainsize']
 exec_times = data.iloc[:, 1:].mean(axis='columns')
 
-# show plot
-sns.lineplot(x=grainsize, y=exec_times)
-plt.xlabel('Grainsize')
-plt.ylabel('Execution time (μs)')
-plt.grid(linestyle='--', linewidth=0.2)
-plt.show()
 # save plot as png
-sns.lineplot(x=grainsize, y=exec_times)
-plt.xlabel('Grainsize')
-plt.ylabel('Execution time (μs)')
-plt.grid(linestyle='--', linewidth=0.2)
-plt.savefig('./images/grainsize.png', dpi=200, format='png', bbox_inches='tight')
+if sys.argv[-1] == "save":
+    sns.lineplot(x=grainsize, y=exec_times)
+    plt.xlabel('Grainsize')
+    plt.ylabel('Execution time (μs)')
+    plt.grid(linestyle='--', linewidth=0.2)
+    plt.savefig('./images/grainsize.png', dpi=200, format='png', bbox_inches='tight')
+# show plot
+elif sys.argv[-1] == "show":
+    sns.lineplot(x=grainsize, y=exec_times)
+    plt.xlabel('Grainsize')
+    plt.ylabel('Execution time (μs)')
+    plt.grid(linestyle='--', linewidth=0.2)
+    plt.show()
+else:
+    print("Usage: python3 plot.py [save|show]")
